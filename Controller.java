76b69648f6bcdbb4f014e4538085cf93e40c3453
jakubvasinsky <55527876+jakubvasinsky@users.noreply.github.com>
2020-02-27 14:00:32 +0100

    Add files via upload

--- Controller.java
+++ Controller.java
@@ -1,8 +1,11 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class Controller {
+
     public Button btn0;
     public Button btn1;
     public Button btn2;
@@ -19,4 +22,41 @@ public class Controller {
     public Button btn13;
     public Button btn14;
     public Button btn15;
+    public Button btnnewgame;
+    public Button btnendgame;
+    public Button btnrestartgame;
+
+    public void clickBtn0(ActionEvent actionEvent) {
+        System.out.println(((Button)actionEvent.getSource()).getId());
+        String temp = ((Button)actionEvent.getSource()).getId().substring(3);
+        int value = Integer.parseInt(temp);
+        //game.nextMove(value);
+
+    }
+    public void rePaint(){
+        /*Tile[][] arr=game.getField().getArr();
+        if (arr==null)
+            return;
+        btn0.setText(arr[][].isValue()?"X":"O");*/
+    }
+
+    public void clickNewGame(ActionEvent actionEvent) {
+        // game.startgame
+        btnnewgame.setDisable(true);
+        btnendgame.setDisable(false);
+        btnrestartgame.setDisable(false);
+    }
+
+    public void clickEndGame(ActionEvent actionEvent) {
+        Stage stage =(Stage) btnendgame.getScene().getWindow();
+        stage.close();
+    }
+
+    public void clickRestartGame(ActionEvent actionEvent) {
+
+      //  game.resetGame();
+
+        btnrestartgame.setDisable(true);
+        btnnewgame.setDisable(false);
+    }
 }
